1dd5dea36d8601a6b3f69849cd821f344a34b2d4
Ludwig Lewis <ludwig.lewis03@gmail.com>
2024-11-11 21:03:32 +0100

    Fixed so it works

--- 34bot.py
+++ 34bot.py
@@ -12,43 +12,51 @@ GUILD = os.getenv("DISCORD_GUILD")
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
-client = discord.Client(intents=intents)
+# client = discord.Client(intents=intents)
 bot = commands.Bot(intents=intents, command_prefix="/")
 
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
+    for guild in bot.guilds:
         if guild.name == GUILD:
             break
 
     print(
-        f"{client.user} is connected to the following guild:\n"
+        f"{bot.user} is connected to the following guild:\n"
         f"{guild.name}(id: {guild.id})"
     )
     for member in guild.members:
         print(member)
 
 
-@client.event
+@bot.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == bot.user:
         return
     if message.content.startswith("Pung"):
         await message.channel.send("Hello!")
     if message.author.id == 522004874083172363:
         await message.reply("Opp")
+    await bot.process_commands(message)
 
 
 @bot.command(name="count")
 async def nine_nine(ctx, arg):
     for i in range(int(arg)):
         if i + 1 == 34:
-            await ctx.channel.send(":34_an:")
+            await ctx.channel.send("<:34_an:1305515512578441256>")
         else:
             await ctx.channel.send(i + 1)
         time.sleep(0.1)
 
 
-# client.run(TOKEN)
+@bot.command(name="34")
+async def thirtyfour(ctx, arg):
+    out = ""
+    for i in range(int(arg)):
+        out += "<:34_an:1305515512578441256>"
+    await ctx.channel.send(out)
+
+
 bot.run(TOKEN)
