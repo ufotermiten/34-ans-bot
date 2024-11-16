b26f8bd4f4c236b8f553d02bd42fae2fbea4d996
Elliot Duchek <elliot.duchek@gmail.com>
2024-11-16 13:42:15 +0100

    👽️ add count command

--- commands/utility/count.js
+++ commands/utility/count.js
@@ -4,14 +4,21 @@ const sleep = require('../../util/sleep');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('count')
-		.setDescription('Skriv hur högt du vill att boten ska räkna'),
-	async execute(interaction, arg) {
-		for (let i = 1; i < arg; i++) {
+		.setDescription('Skriv hur högt du vill att boten ska räkna')
+		.addNumberOption(option =>
+			option
+				.setName('siffra')
+				.setDescription('Hur högt du vill räkna')
+				.setRequired(true)),
+	async execute(interaction) {
+		const countTo = interaction.options.getNumber('siffra');
+		interaction.reply({ content: `Okej, om du envisas kan jag väl räkna till ${countTo} åt dig...` });
+		for (let i = 1; i <= countTo; i++) {
 			if (i === 34) {
 				await interaction.channel.send('<:34_an:1305515512578441256>');
 			}
 			else {
-				await interaction.channel.send(i);
+				await interaction.channel.send(`${i}`);
 			}
 			await sleep(100);
 		}
