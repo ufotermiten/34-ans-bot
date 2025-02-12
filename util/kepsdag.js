894020e370cf18709265c0a4cb4aa2f5c84cf3eb
Elliot Duchek <elliot.duchek@gmail.com>
2025-02-12 10:56:44 +0100

    ✨ make kepsdag work with csv files

--- util/kepsdag.js
+++ util/kepsdag.js
@@ -1,32 +1,29 @@
 const fs = require('node:fs');
-const kepsDaysFile = './data/kepsdagar.json';
+const Papa = require('papaparse');
+
+const kepsDaysFile = './data/kepsdagar.csv';
 
 module.exports = () => {
-	const today = new Date();
-	const kepsData = JSON.parse(fs.readFileSync(kepsDaysFile));
-	for (const kepsDay of kepsData.days) {
-		const kepsDate = new Date(kepsDay.date);
+	const today = new Date().toISOString().slice(0, 10);
+	// const kepsData = JSON.parse(fs.readFileSync(kepsDaysFile));
+	const kepsDays = Papa.parse(fs.readFileSync(kepsDaysFile, 'utf8'), { header: true }).data;
+	for (const kepsDay of kepsDays) {
 		// if recurring kepsdag, check if days and months match
-		if (kepsDay.recurring) {
-			if (dayMonthEq(today, kepsDate)) return kepsDay.reason;
+		if (kepsDay.recurring == 'true') {
+			if (dayMonthEq(today, kepsDay.date)) return kepsDay.reason;
 		}
 		// else include year
-		else if (eq(today, kepsDate)) {return kepsDay.reason;}
+		else if (eq(today, kepsDay.date)) {return kepsDay.reason;}
 	}
 	return null;
 };
 
-// converts dates to ISO-strings (YYYY-MM-DDandotherstuff) and checks if months and day
-// portions match
+// given date like YYYY-MM-DD, check if month and day portions match
 function dayMonthEq(date1, date2) {
-	const ds1 = date1.toISOString();
-	const ds2 = date2.toISOString();
-	return ds1.slice(5, 10) == ds2.slice(5, 10);
+	return date1.slice(5, 10) == date2.slice(5, 10);
 }
 
 // same but including year
 function eq(date1, date2) {
-	const ds1 = date1.toISOString();
-	const ds2 = date2.toISOString();
-	return ds1.slice(0, 10) == ds2.slice(0, 10);
+	return date1 == date2;
 }
