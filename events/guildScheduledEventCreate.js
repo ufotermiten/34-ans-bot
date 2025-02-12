6c572f64b47f13b49c9379a3f7e3f5067809b1ef
Elliot Duchek <elliot.duchek@gmail.com>
2025-02-12 13:25:17 +0100

    🐛 make the kepsdag reminder use the event scheduled start instead of todays date, add error handling

--- events/guildScheduledEventCreate.js
+++ events/guildScheduledEventCreate.js
@@ -9,8 +9,16 @@ module.exports = {
 	execute(event) {
 		const eventInfo = (event.name + event.description).toLowerCase();
 		if (eventInfo.includes('sittning')) {
-			const date = new Date().toISOString().slice(0, 10);
-			updateKepsdagar({ date, recurring: false, reason: `Det är ${event.name}` });
+			const date = new Date(event.scheduledStartTimestamp).toISOString().slice(0, 10);
+			try {
+				updateKepsdagar({ date, recurring: false, reason: `Det är ${event.name}` });
+			}
+			catch (err) {
+				console.error(err);
+				event.client.channels.cache.get(process.env.BOT_DEV_CHANNEL_ID)
+					.send(`<@&${process.env.BOT_DEV_ROLE_ID}> scheduled sittning-event created`
+            + ' but keps reminder creation failed. :ogre:');
+			}
 		}
 	},
 };
